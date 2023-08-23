fcaa09388a5b0e90af2984f1743a85a77a953578
muhamadhaspin <muhamadhaspin@gmail.com>
2023-08-23 14:35:39 +0700

    --cg : add flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './public/**/*.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        './node_modules/flowbite/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-
